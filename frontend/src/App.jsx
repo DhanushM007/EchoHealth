b25dc44e9a0aae0b5011ee4a02c852bb456ef0f2
DhanushM007 <117370218+DhanushM007@users.noreply.github.com>
2026-05-18 23:19:27 +0530

    Initial EchoHealth setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App(){
+  
+  const [message,setMessage]=useState("");
+  useEffect(()=>{
+    fetch('http://127.0.0.1:5000/')
+    .then(res=>res.json())
+    .then(data=>setMessage(data.message))
+  },[]);
+
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">{message}</h1>
+    </div>
+  );
+}
+export default App;
---
+++ readme.md
@@ -0,0 +1,12 @@
+# EchoHealth
+
+EchoHealth is a full-stack AI-powered healthcare platform that predicts diseases based on user symptoms using Machine Learning and provides intelligent healthcare assistance through Generative AI.
+
+The project aims to improve traditional disease prediction systems by introducing interactive symptom analysis, explainable predictions, and context-aware AI assistance.
+
+## Tech Stack
+- React
+- Flask
+- MongoDB
+- Machine Learning
+- Generative AI
